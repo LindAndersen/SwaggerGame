df1b30e89c3adfdea5d6382169d947db8391209f
ulhen <umh@outlook.dk>
2025-03-20 13:20:48 +0100

    questwork
    nicers

--- HorrorFX/QuestItem/src/main/java/module-info.java
+++ HorrorFX/QuestItem/src/main/java/module-info.java
@@ -4,6 +4,7 @@ import dk.sdu.smp4.QuestItemPlugin;
 module QuestItem {
     requires CommonInteractable;
     requires Common;
+    requires java.desktop;
     provides dk.sdu.smp4.common.Services.IGamePluginService with QuestItemPlugin;
     provides dk.sdu.smp4.common.interactable.Services.IQuestInteractable with QuestItemInteractionSystem;
 
