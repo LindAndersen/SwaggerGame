7a594b22cb57735f5c31b78bc68de7ff2334d678
ulhen <umh@outlook.dk>
2025-03-27 13:07:29 +0100

    Hehe

--- HorrorFX/QuestItem/src/main/java/dk/sdu/smp4/QuestItemPlugin.java
+++ HorrorFX/QuestItem/src/main/java/dk/sdu/smp4/QuestItemPlugin.java
@@ -12,6 +12,7 @@ public class QuestItemPlugin implements IGamePluginService {
     @Override
     public void start(GameData gameData, World world) {
         world.addEntity(CreateQuest("name", "description", 1, 20, 20, 8, 100, 140));
+        world.addEntity(CreateQuest("name2", "description2", 1, 40, 40, 8, 100, 140));
     }
 
     @Override
@@ -21,7 +22,7 @@ public class QuestItemPlugin implements IGamePluginService {
 
     private Entity CreateQuest(String questName, String questDescription, int questID, int questX, int questY, int questRadius, int questCompX, int questCompY) {
 
-        Entity quest = new QuestItem(questName, questDescription, questID, questX, questY, questRadius, questCompX, questCompY);
+        SoftEntity quest = new QuestItem(questName, questDescription, questID, questX, questY, questRadius, questCompX, questCompY);
         quest.setPolygonCoordinates(-5,-5,5,0,-5,5, 10, 10);
 
         quest.setX(questX);
