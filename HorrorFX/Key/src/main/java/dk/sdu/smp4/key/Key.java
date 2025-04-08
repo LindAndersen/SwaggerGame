f6585574efbcbf33a7d5de095f60154ef361484d
LindAndersen <andersen.flan@gmail.com>
2025-04-08 22:01:57 +0200

    Added sprites for key and quest, made softentity polygons 0% opacity

--- HorrorFX/Key/src/main/java/dk/sdu/smp4/key/Key.java
+++ HorrorFX/Key/src/main/java/dk/sdu/smp4/key/Key.java
@@ -1,7 +1,49 @@
 package dk.sdu.smp4.key;
 
 import dk.sdu.smp4.common.data.Entity;
+import dk.sdu.smp4.common.data.SoftEntity;
+import javafx.scene.image.Image;
 
-public class Key extends Entity {
+import java.util.HashMap;
+import java.util.Map;
+
+public class Key extends SoftEntity {
+    private final Image bronzeKey = new Image(getClass().getResourceAsStream("/bronze.png"),20, 20, true, true);
+    private final Image goldKey = new Image(getClass().getResourceAsStream("/gold.png"),20, 20, true, true);
+    private final Image greyKey = new Image(getClass().getResourceAsStream("/grey.png"),20, 20, true, true);
+    private final Image silverKey = new Image(getClass().getResourceAsStream("/silver.png"),20, 20, true, true);
+
+    private Map<String, Object> properties = new HashMap<>();
+
+    public Key()
+    {
+
+    }
+
+    public Key(String keyId)
+    {
+        setProperties("keyId", keyId);
+        if(keyId.toLowerCase().contains("bronze"))
+        {
+            setImage(bronzeKey);
+        }else if (keyId.toLowerCase().contains("gold"))
+        {
+            setImage(goldKey);
+        } else if (keyId.toLowerCase().contains("grey")) {
+            setImage(greyKey);
+        } else
+        {
+            setImage(silverKey);
+        }
+    }
+
+
+    public Object getProperty(String key) {
+        return properties.get(key);
+    }
+
+    public void setProperties(String key, Object value){
+        properties.put(key, value);
+    }
 }
 
