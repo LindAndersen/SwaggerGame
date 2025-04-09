84c22ff2462d73081a017501c3e5c194856e4fab
EmilSlotsvang <emil.slotsvang@gmail.com>
2025-04-09 13:12:13 +0200

    Hp bare + game over
    jaaa du læste rigtigt

--- HorrorFX/Core/src/main/java/dk.sdu.smp4.main/Main.java
+++ HorrorFX/Core/src/main/java/dk.sdu.smp4.main/Main.java
@@ -4,9 +4,13 @@ import dk.sdu.smp4.common.Services.IEntityProcessingService;
 import dk.sdu.smp4.common.Services.IGamePluginService;
 import dk.sdu.smp4.common.Services.IPostEntityProcessingService;
 import dk.sdu.smp4.common.data.*;
+import dk.sdu.smp4.common.events.EventBus;
+import dk.sdu.smp4.common.events.GameOverEvent;
+import dk.sdu.smp4.common.events.UpdateHUDLifeEvent;
 import dk.sdu.smp4.common.lightsource.data.CommonLightSource;
 import javafx.animation.AnimationTimer;
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
@@ -22,6 +26,7 @@ import javafx.scene.image.WritableImage;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
@@ -128,7 +133,15 @@ public class Main extends Application {
 
         getPluginServices().forEach(plugin -> plugin.start(gameData, world));
 
+        initLifeHUD();
+
         render();
+        EventBus.subscribe(GameOverEvent.class, event -> {
+            showGameOverScreen();
+        });
+        EventBus.subscribe(UpdateHUDLifeEvent.class, event -> {
+            updateLifeHUD(event.getLives());
+        });
     }
 
     private void render() {
@@ -143,6 +156,7 @@ public class Main extends Application {
             }
         }.start();
     }
+
 
     private void update() {
         getEntityProcessingServices().forEach(service -> service.process(gameData, world));
@@ -279,5 +293,68 @@ public class Main extends Application {
 
     private Collection<? extends IPostEntityProcessingService> getPostEntityProcessor() {
         return ServiceLoader.load(IPostEntityProcessingService.class).stream().map(ServiceLoader.Provider::get).collect(Collectors.toList());
+    }
+
+    private void showGameOverScreen() {
+        VBox gameOverBox = new VBox();
+        gameOverBox.setPrefSize(gameData.getDisplayWidth(), gameData.getDisplayHeight());
+        gameOverBox.setStyle("-fx-background-color: rgba(0, 0, 0, 0.85);");
+        gameOverBox.setAlignment(Pos.CENTER);
+        gameOverBox.setSpacing(20);
+
+        Label gameOverLabel = new Label("Game Over");
+        gameOverLabel.getStyleClass().add("title-label");
+
+        Button goAgainButton = new Button("Go Again");
+        goAgainButton.getStyleClass().add("menu-button");
+        goAgainButton.setOnAction(e -> {
+            gameData.getTextLayer().getChildren().remove(gameOverBox);
+            restartGame();
+        });
+
+        Button quitButton = new Button("Quit");
+        quitButton.getStyleClass().add("menu-button");
+        quitButton.setOnAction(e -> System.exit(0));
+
+        gameOverBox.getChildren().addAll(gameOverLabel, goAgainButton, quitButton);
+        Platform.runLater(() -> {
+            gameData.getTextLayer().getChildren().add(gameOverBox);
+        });
+        gameData.setPaused(true);
+
+        gameOverBox.getStylesheets().add(getClass().getResource("/styles/style.css").toExternalForm());
+    }
+
+    private void restartGame() {
+        world.getEntities().forEach(entity -> gameData.getPolygonLayer().getChildren().clear());
+        polygons.clear();
+        images.clear();
+        world.getEntities().clear();
+
+        getPluginServices().forEach(plugin -> plugin.stop(gameData, world));
+        getPluginServices().forEach(plugin -> plugin.start(gameData, world));
+
+        gameData.setPaused(false);
+    }
+    private final HBox lifeBox = new HBox(5);
+    private final int maxLives = 2;
+
+    private void initLifeHUD() {
+        lifeBox.setPadding(new Insets(10));
+        lifeBox.setAlignment(Pos.TOP_LEFT);
+        updateLifeHUD(maxLives);
+
+        gameData.getTextLayer().getChildren().add(lifeBox);
+    }
+
+    private void updateLifeHUD(int lives) {
+        lifeBox.getChildren().clear();
+        for (int i = 0; i < maxLives; i++) {
+            Image heartImage = new Image(getClass().getResourceAsStream(
+                    i < lives ? "/images/heart_full.png" : "/images/heart_empty.png"
+            ), 32, 32, true, true);
+            ImageView heartView = new ImageView(heartImage);
+            lifeBox.getChildren().add(heartView);
+        }
     }
 }
