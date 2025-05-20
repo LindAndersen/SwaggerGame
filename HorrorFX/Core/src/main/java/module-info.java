0df92a1d758e4f95616a309be30fd722f7c40c2a
LindAndersen <andersen.flan@gmail.com>
2025-05-21 00:49:55 +0200

    Removed unused dependencies from Core module

--- HorrorFX/Core/src/main/java/module-info.java
+++ HorrorFX/Core/src/main/java/module-info.java
@@ -1,13 +1,5 @@
-import dk.sdu.smp4.common.Services.GameLoop.IEntityProcessingService;
-import dk.sdu.smp4.common.Services.GameLoop.IPostEntityProcessingService;
-
 module Core {
-    requires CommonLightSource;
-    requires CommonPlayerLight;
-    requires Common;
     requires CommonGUIElements;
     requires javafx.controls;
     opens dk.sdu.smp4.main to javafx.graphics;
-    uses IEntityProcessingService;
-    uses IPostEntityProcessingService;
 }
